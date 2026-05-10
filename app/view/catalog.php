a239dfb6eec8c422f1de3012762c29102beb3744
Alysson Andrade <alyssonandrade186@gmail.com>
2026-05-10 16:54:27 -0300

    Layout: Organização de Elementos de CSS do projeto e adição de elementos em Catalog.php

--- app/view/catalog.php
+++ app/view/catalog.php
@@ -1,118 +1,114 @@
-<style>
-    ul {
-        list-style: none;
-    }
-
-    .container-section {
-        max-width: 1310px;
-        margin: 0 auto;
-    }
-
-    .container-filtro {
-        width: 250px;
-        padding: 15px;
-        height: 900px;
-        background-color: #f2f5f7;
-    }
-
-    .container-catalog {
-        display: flex;
-        gap: 17px;
-        margin: 10px 0;
-    }
-
-    .container-card {
-        width: 100%;
-        padding: 15px;
-        background-color: #d5e7f7;
-        height: auto;
-    }
-
-    .container-option-catalog {
-        width: 100%;
-        height: 70px;
-        display: flex;
-        justify-content: space-between;
-    }
-
-    .row-option-right {
-        width: 350px;
-        display: flex;
-    }
-
-    .option-btn-catalog {
-        width: 35px;
-        height: 35px;
-        padding: 5px;
-    }
-
-
-    .option-select-catalog {
-        width: 130px;
-        height: 35px;
-        padding: 5px;
-    }
-
-    .row-calalog {
-        display: flex;
-        /* Ativa o modo Flexbox */
-        flex-wrap: wrap;
-        /* Faz os cards descerem quando o limite é atingido */
-        gap: 10px;
-        /* Cria um espaçamento entre os cards */
-        width: 100%;
-    }
-
-    .container-paginacao {
-        width: 100%;
-        height: 30px;
-        padding: 10px;
-        background-color: #d5e7f7;
-        margin: 0 auto;
-    }
-
-    .container-botoes-paginacao {
-        display: flex;
-        justify-content: center;
-    }
-</style>
-
-
-<section id="catalog">
+<section class="section-catalog" id="catalog">
     <div class="container-section">
-        <div class="container-catalog">
-            <div class="container-filtro">
-                <h1>Filtros Em Breve</h1>
-            </div>
-            <div class="container-card">
-                <div class="container-option-catalog">
-                    <div class="row-option-left"><span>700 imoveis em Pernabuens</span></div>
+        <div class="header-catalog">
+            <h1>Encontre o imóvel Ideal</h1>
+            <span>Use os filtros a baixo para encontrar o melhor imóvel para você</span>
+            <div class="container-header">
+                <div class="container-input-header">
+                    <span>Localização</span>
+                    <input class="input-default input-header input-header-first" placeholder="Localização" type="text" data-filter="bairro">
+                </div>
+                <div class="container-input-header">
+                    <span>Categoria</span>
+                    <input class="input-default input-header" placeholder="Categoria" type="text" id="input-category" >
+                </div>
+                <div class="container-input-header">
+                    <span>Tipo do Imovel</span>
+                    <input class="input-default input-header" placeholder="Tipo do Imovel" type="text" id="input-type" data-filter="tipo">
+                </div>
+                <div class="container-input-header">
+                    <span>Finalidade</span>
+                    <input class="input-default input-header" placeholder="Finalidade" type="text" id="input-modality" data-filter="modalidade" >
+                </div>
+                <div class="container-input-header">
+                    <button class="btn-default btn-blue btn-header-catalog" id="btn-filter-header">Buscar</button>
+                </div>
 
-                    <div class="row-option-right">
-                        <div class="container-option-catalog">
-                            <span>Ordenar por: </span>
-                            <select class="option-select-catalog" name="" id="">
-                                <option value="" hidden>Recentes</option>
-                            </select>
-                            <div class="row-btn-catalog">
-                                <button class="option-btn-catalog"><i class="fa-solid fa-border-all"></i></button>
-                                <button class="option-btn-catalog"><i class="fa-solid fa-list"></i></button>
+
+            </div>
+            <div class="container-catalog">
+                <div class="container-filtro">
+                    <div class="filtro-superior">
+                        <h3>Complementos do Imovel</h3>
+                        <small>Nós conte do que está precisando.</small>
+                        <div class="filtro-tags">
+
+                        </div>
+                        <div class="container-btn">
+                            <button class="btn-default btn-blue">Adicionar Filtros</button>
+                            <button class="btn-default btn-white">Limpar Filtros</button>
+                        </div>
+                    </div>
+                    <div class="filtro-inferior">
+                        <div class="row-filtro-inferior">
+                            <div class="input-range">
+                                <span>Número de quartos</span>
+                                <input type="range" name="" id="" min="0" value="0" max="5">
+                                <div class="row-numeros-range">
+                                    <small>0</small>
+                                    <small>1</small>
+                                    <small>2</small>
+                                    <small>3</small>
+                                    <small>4</small>
+                                    <small>+5</small>
+                                </div>
+                            </div>
+                            <div class="input-range">
+                                <span>Número de banheiros</span>
+                                <input type="range" name="" id="" min="0" value="0" max="5">
+                                <div class="row-numeros-range">
+                                    <small>0</small>
+                                    <small>1</small>
+                                    <small>2</small>
+                                    <small>3</small>
+                                    <small>4</small>
+                                    <small>+5</small>
+                                </div>
+                            </div>
+                            <div class="input-range">
+                                <span>Número de garagem</span>
+                                <input type="range" name="" id="" min="0" value="0" max="5">
+                                <div class="row-numeros-range">
+                                    <small>0</small>
+                                    <small>1</small>
+                                    <small>2</small>
+                                    <small>3</small>
+                                    <small>4</small>
+                                    <small>+5</small>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="row-calalog">
-                    <?php require_once COMPONENTS_PATH . '/imovel/card.php'; ?>
+                <div class="container-card">
+                    <div class="container-option-catalog">
+                        <div class="row-option-left"><span class="option-text-catalog"></span></div>
+
+                        <div class="row-option-right">
+                            <div class="container-option-catalog">
+                                <span>Ordenar por: </span>
+                                <select class="option-select-catalog" name="" id="">
+                                    <option value="" hidden>Recentes</option>
+                                </select>
+                                <div class="row-btn-catalog">
+                                    <button class="option-btn-catalog"><i class="fa-solid fa-border-all"></i></button>
+                                    <button class="option-btn-catalog"><i class="fa-solid fa-list"></i></button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row-calalog">
+                        <?php require_once COMPONENTS_PATH . '/imovel/card.php'; ?>
+                    </div>
+                </div>
+            </div>
+            <div class="container-paginacao">
+                <div class="lista-paginacao">
+                    <ul class="container-botoes-paginacao">
+                    </ul>
                 </div>
             </div>
         </div>
-        <div class="container-paginacao">
-            <div class="lista-paginacao">
-                <ul class="container-botoes-paginacao">
-                </ul>
-            </div>
-        </div>
-    </div>
 
 </section>
 
