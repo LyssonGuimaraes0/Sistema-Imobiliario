8cd0dc8ee3802e6618e3b03f418aedb8f3e4a21e
Alysson Andrade <alyssonandrade186@gmail.com>
2026-05-10 16:57:10 -0300

    Draft: Inicio de prototipo de Navabar

--- app/view/home.php
+++ app/view/home.php
@@ -1,1 +1,16 @@
-<h1>HOME</h1>
+<!-- <h1>HOME</h1> -->
+<header>
+    <div class="logo">
+        <img src="<?= IMAGEM_URL ?>/placeholder/image-not-found.png" alt="Image-not-found">
+        <h1>MeuProjeto</h1>
+    </div>
+
+    <nav>
+        <ul>
+            <li><a href="#">Início</a></li>
+            <li><a href="#">Catálogo</a></li>
+            <li><a href="#">Sobre</a></li>
+            <li><a href="#">Contato</a></li>
+        </ul>
+    </nav>
+</header>
